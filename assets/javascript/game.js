9ea802a80d844c5216f2045bf3c165d68d199ba3
Kyle Knox <kyleknox86@gmail.com>
2019-03-30 21:44:29 -0600

    did things right this time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,123 @@
-// Array of possible answers
-var answerList = ["Kirk", "Spock", "Picard", "Riker", "Sulu", "Ohura", "Scotty", "LaForge", "Worf", "Sisko", "Janeway", "Tuvok", "Nelix", "Dax"];
-//Empty object for incorrect guesses
+// GLOBAL VARIABLES
+//--------------------------------------------------------------------------------------
+//Arrays and Varibles
+var wordOptions = ["kirk", "spock", "picard", "riker", "sulu", "ohura", "scotty", "laforge", "worf", "sisko", "janeway", "tuvok", "nelix", "dax"];
+var selectedWord = "";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = []; //_ _ _ _ _ 
+var wrongLetters = [];
 
-var wrongGuesses= {};
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+//FUNCTIONS
+//------------------------------------------
+
+function startGame() {
+  selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+  lettersInWord = selectedWord.split("");
+  numBlanks = lettersInWord.length;
+
+  //Reset
+  guessesLeft = 9;
+  wrongLetters = [];
+  blanksAndSuccesses = [];
+
+  //Populate Blanks and Successes
+  for (var i = 0; i < numBlanks; i++) {
+    blanksAndSuccesses.push(" _");
+  }
+
+  //Change html
+  document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("");
+  document.getElementById("numGuesses").innerHTML = guessesLeft;
+  document.getElementById("winCounter").innerHTML = winCount;
+  document.getElementById("lossCounter").innerHTML = lossCount;
 
 
-// computer chooses one from array
+  //Testing/Debugging
+  console.log(selectedWord);
+  console.log(lettersInWord);
+  console.log(numBlanks);
+  console.log(blanksAndSuccesses);
 
-var computerChoice = answerList[Math.floor(Math.random() * answerList.length)];
-//Create spaces for chosen answer
-function createLetterSpace() {
-    for(var i =0; i < computerChoice.length; i++) {
-            var letterSpace = document.createElement("P");
-            letterSpace.setAttribute("letter", computerChoice[i]);
-            document.getElementById("contentArea").appendChild(letterSpace);
+
+
+}
+
+function checkLetters(letter) {
+  // check if letter exists in word
+
+  var isLetterInWord = false;
+
+  for (var i = 0; i < numBlanks; i++) {
+    if (selectedWord[i] == letter) {
+      isLetterInWord = true;
 
     }
+  }
+  // Check where in word letter exists, then populate blank spaces
+  if (isLetterInWord) {
+    for (var i = 0; i < numBlanks; i++) {
+      if (selectedWord[i] == letter) {
+        blanksAndSuccesses[i] = letter;
+      }
+    }
+  }
+  //Letter wasnt found
+  else {
+    wrongLetters.push(letter);
+    guessesLeft--;
+  }
+  console.log(blanksAndSuccesses);
+
 }
+
+function roundComplete() {
+  console.log("Win Count: " + winCount + "| Loss Count: " + lossCount + " | Guesses Left " + guessesLeft);
+
+  // Update the HTML  to reflect the most recent count stats
+  document.getElementById("numGuesses").innerHTML = guessesLeft;
+  document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+  document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+
+  // Check if user won
+  if (lettersInWord.toString() == blanksAndSuccesses.toString()) {
+    winCount++;
     
-        //Give spaces chosen value
+
+    //Update the win counter in the HTML
+    document.getElementById("winCounter").innerHTML = winCount;
 
 
-//User enters key
+    setTimeout(startGame, 500);
+  }
+  //Check if user lost
+  else if (guessesLeft == 0) {
+    lossCount++;
+   
 
-        //decrement allowed guesses 
+    //Update the HTML
+    document.getElementById("lossCounter").innerHTML = lossCount;
 
-        //compare computer choice to user's entered value
+    setTimeout(startGame, 3000);
+  }
+}
 
-        // if yes, log value to answer space
-            //check if win
-        // if no, log value to incorrect choices
-            //check if loss
+//MAIN PROCESS
+//------------------------------------------------------------
+// Initiates code for the first time
+startGame();
 
+// Register Keyclicks
+document.onkeyup = function (event) {
+  var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+  checkLetters(letterGuessed);
+  roundComplete();
 
-//check if win
-    //check if all characters in word are displayed
-        //if win, increment wins and reset game
-        //if not, keep going
+}
 
-
- //check if loss
-
-    //check if guesses remaining = 0
-        //if loss, increment losses and reset game
-        //if not, keep going
-
-//reset game after win/loss
-
-    //clear answer space
-    //choose a new word from array
-    //clear incorrect guesses
-    //reset allowed guesses
-
-//reset button to clear game
-    //resets wins/losses as well
-
-
-
-
-
-
-
-
-
+//Testing/Debugging
